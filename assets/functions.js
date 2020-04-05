492876c2a9bab214d8dde191af09fedae029d51c
nina.ruizandres@gmail.com <Atento02>
2020-04-05 13:59:59 +0200

    Add initialize and loadData functions, display on html data of .json

--- assets/functions.js
+++ assets/functions.js
@@ -1,4 +1,22 @@
 /**
  * @todo to add functions to make works the front
  */
+const loadData = () => {
+    $.get('api/user.json', function (data) {
+        $('#user-name').html(data.name);
+        $('#user-last-name').html(data.lastName);
+        $('#user-phones').html('<li>'+data.phones[0]+'</li><li>'+data.phones[1]+'</li>');
+    });
+};
+
+const initialize = () => {
+    loadData();
+    console.log('init')
+};
+
+initialize();
+
+
+
+
 
